51b785ec861879d45c3fc9c5b41f05e895cb79fe
Ibsy Matute <ibsy@rocketmail.com>
2022-08-29 16:14:08 -0500

    Move count state from buttons to App and add Names state that is an array

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Button from "./components/Button";
 import Navbar from "./components/Navbar";
 
@@ -9,13 +10,35 @@ function App(props) {
         {color: 'success', step: 100},
         {color: 'danger', step: 1000}
     ]
+
+    const [count, setCount] = useState(0)
+    const [names, setNames] = useState([])
+    function handleClick(step){
+        console.log('Clicked')
+        setCount(count + step)
+    }
+
+    function handleNameClick(event){
+        event.preventDefault()
+        const name = event.target.firstName.value
+        let newNames = [...names, name]
+        setNames(newNames)
+    }
+
     return (
         <>
-        <Navbar name = 'Brian' city = 'Chicago' />
-        <div className = 'container'>
-            <h1 className = 'text-center'>Hello World!</h1>
-            {buttons.map((b,i) => <Button color = {b.color} step = {b.step} key = {i} />)}
-        </div> 
+            <Navbar name = 'Brian' city = 'Chicago' />
+            <div className = 'container'>
+                <h1 className = 'text-center'>Hello World!</h1>
+                <h3 className = 'text-center'>Total: {count}</h3>
+                {buttons.map((b,i) => <Button color = {b.color} step = {b.step} 
+                    key = {i} handleClick = {handleClick}/>)}
+                <form onSubmit = {handleNameClick}>
+                    <input type='text' className = 'form-control' name = 'firstName' />
+                    <input type = 'submit' value = 'Submit' />
+                </form>
+                {names.map((n, idx) => <p key = {idx}>{n}</p>)}
+            </div> 
         </> 
     );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 export default function Button(props) {
-    console.log('hello this is the button function running')
-    const [count, setCount] = useState(0)
-    function handleClick(){
-        setCount(count + props.step)
-    }
+    
     return (
         <button className = {`btn btn-${props.color} w-100`}
-            onClick={handleClick}>+{props.step} - {count}</button>
+            onClick={() => props.handleClick(props.step)}>+{props.step}</button>
     )
 }
