12d496df558a7ea3e931496df9f439f6186eb651
Ibsy Matute <ibsy@rocketmail.com>
2022-08-30 13:57:07 -0500

    add useEffect for getting racer data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Button from "./components/Button";
 import Navbar from "./components/Navbar";
 
@@ -12,17 +12,30 @@ function App(props) {
     ]
 
     const [count, setCount] = useState(0)
-    const [names, setNames] = useState([])
+    const [racers, setRacers] = useState([])
+
+    // Create an effect -> function to execure after every render
+    useEffect(() => {
+        console.log('useEffect effect callback executed')
+        fetch('http://ergast.com/api/f1/2022/1/driverStandings.json')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                let racerStandings = data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+                setRacers(racerStandings)
+            })
+    }, []) // [] - dependency will only do this effect if state has changed
+            // if the dependency is [] empty array your effect doesn't depend on 
+            // any values from props or state, so it never needs to re-run
+
     function handleClick(step){
         console.log('Clicked')
         setCount(count + step)
     }
 
-    function handleNameClick(event){
+    function handleRacerSubmit(event){
         event.preventDefault()
-        const name = event.target.firstName.value
-        let newNames = [...names, name]
-        setNames(newNames)
+        console.log(event)
     }
 
     return (
@@ -33,11 +46,11 @@ function App(props) {
                 <h3 className = 'text-center'>Total: {count}</h3>
                 {buttons.map((b,i) => <Button color = {b.color} step = {b.step} 
                     key = {i} handleClick = {handleClick}/>)}
-                <form onSubmit = {handleNameClick}>
-                    <input type='text' className = 'form-control' name = 'firstName' />
+                <form onSubmit = {handleRacerSubmit}>
+                    <input type='text' className = 'form-control' name = 'season' />
                     <input type = 'submit' value = 'Submit' />
                 </form>
-                {names.map((n, idx) => <p key = {idx}>{n}</p>)}
+                {racers.map((r, idx) => <p key = {idx}>{r.points}</p>)}
             </div> 
         </> 
     );
